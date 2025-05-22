8268b50281e783607d3df3f730ae0cd378bad8f5
Ayushi-sax <saxenayushi31@gmail.com>
2025-05-22 14:05:28 +0530

    Drumming Js

---
+++ Javascript/index10.js
@@ -0,0 +1,71 @@
+let numOfDrum = document.querySelectorAll(".drum").length;
+//Detecting button press
+for(let i =0;i<numOfDrum;i++){
+   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    
+    let btnIdentity = this.innerHTML;
+    makeSound(btnIdentity);
+    animate(btnIdentity);
+})
+}
+
+//detecting key press on keyboard
+document.addEventListener("keypress",function(event){
+   makeSound(event.key);
+   animate(event.key);
+})
+
+
+
+
+
+
+function makeSound(key){
+   switch (key) {
+      case "w":
+         var tom1 = new Audio("sounds/tom-1.mp3");
+         tom1.play();
+         break;
+         case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    
+      default: console.log(btnIdentity);
+    }
+}
+
+function animate(key1){
+   let element = document.querySelector("." + key1);
+ element.classList.add("pressed");
+ setTimeout(function() {
+   element.classList.remove("pressed");
+ }, 100);
+}
+
